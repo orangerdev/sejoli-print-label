c276a4033adee75dbbcac88cb0c0953cae6f76cb
Zakky Ashiddiqi <asd.zakky@gmail.com>
2021-08-17 23:07:40 +0700

    add[variation&resi] display data variation and number resi on print label

--- admin/class-sejoli-print-label-admin.php
+++ admin/class-sejoli-print-label-admin.php
@@ -128,7 +128,7 @@ class Admin {
      * @return  json
      */
 	public function print_shipment_label(){
-
+		
 		$params = wp_parse_args( $_POST, array(
             'orders' => NULL,
             'nonce'  => NULL
@@ -144,119 +144,13 @@ class Admin {
         if( wp_verify_nonce( $params['nonce'], 'sejoli-print-shipment-label') ) :
 
             unset( $params['nonce'] );
-
+  
 	        $response = sejolisa_get_orders(['ID' => $params['orders'] ]);
 
 	        if(false !== $response['valid']) :
-
+	        	
 			  	$html = '';
-			  	$html .= '<html><head><meta charset="utf-8"></head><body>';
-				$html .= '<div class="labelarea" style="width:100%; height: 100%; display:inline-block; margin-top: 0em; width: 100%;">';
-				$count = 1;
-				foreach ($response['orders'] as $value) {
-					$receiver_destination_id   = $value->meta_data['shipping_data']['district_id'];
-        			$receiver_destination_city = $this->get_subdistrict_detail($receiver_destination_id);
-					$shipper_origin_id   	   = $value->product->shipping['origin'];
-        			$shipper_origin_city 	   = $this->get_subdistrict_detail($shipper_origin_id);
-        			$html .= '<div class="label-item" style="width: 88.5mm; height: auto; border: 2px solid #000; margin-top: 0.5em; margin-right: 0.5em; margin-bottom: 5mm; float:left; padding: 10px;">';
-						$html .= '<table style="width: 88.5mm">';
-							$html .= '<thead text-align: left;">';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><b>INV #'.$value->ID.'</b></td>';
-									$html .= '<td style="width: 43.05mm"><b>'.__('Label Pengiriman', 'sejoli-print-label').'</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="height: 4px;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="height: 4px;"></div></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="border-bottom: 1px dashed #999;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="border-bottom: 1px dashed #999;"></div></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="height: 1px;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="height: 1px;"></div></td>';
-								$html .= '</tr>';
-							$html .= '</thead>';
-							$html .= '<tbody>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">'.__('Kurir', 'sejoli-print-label').'</td>';
-									$html .= '<td style="width: 43.05mm"><b>'.$value->meta_data['shipping_data']['courier'].' - '.$value->meta_data['shipping_data']['service'].'</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">'.__('Ongkir', 'sejoli-print-label').'</td>';
-									$html .= '<td style="width: 43.05mm"><b>'.sejolisa_price_format($value->meta_data['shipping_data']['cost']).'</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">'.__('Berat', 'sejoli-print-label').'</td>';
-									$html .= '<td style="width: 43.05mm"><b>'.$value->product->shipping['weight'].' gram</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">'.__('Pembayaran', 'sejoli-print-label').'</td>';
-									$html .= '<td style="width: 43.05mm"><b>'.$value->payment_gateway.'</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">&nbsp;</td>';
-									$html .= '<td style="width: 43.05mm">&nbsp;</td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><b>'.__('PENGIRIM', 'sejoli-print-label').'</b></td>';
-									$html .= '<td style="width: 43.05mm"><b>'.__('PENERIMA', 'sejoli-print-label').'</b></td>';
-								$html .= '</tr>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><b>'.get_bloginfo('name').'</b></td>';
-									$html .= '<td style="width: 43.05mm"><b>'.$value->meta_data['shipping_data']['receiver'].' - '.$value->meta_data['shipping_data']['phone'].'</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">'.$shipper_origin_city['type'].' '.$shipper_origin_city['city'].', '.$shipper_origin_city['subdistrict_name'].', '.$shipper_origin_city['province'].'</td>';
-									$html .= '<td style="width: 43.05mm">'.$value->meta_data['shipping_data']['address'].', '.$receiver_destination_city['type'].' '.$receiver_destination_city['city'].', '.$receiver_destination_city['subdistrict_name'].', '.$shipper_origin_city['province'].'</td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="height: 4px;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="height: 4px;"></div></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="border-bottom: 1px dashed #999;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="border-bottom: 1px dashed #999;"></div></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="height: 1px;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="height: 1px;"></div></td>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><b>'.__('QTY', 'sejoli-print-label').'</b></td>';
-									$html .= '<td style="width: 43.05mm"><b>'.__('PRODUK', 'sejoli-print-label').'</b></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">'.$value->quantity.'</td>';
-									$html .= '<td style="width: 43.05mm">'.$value->product_name.'</td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="height: 4px;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="height: 4px;"></div></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="border-bottom: 1px dashed #999;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="border-bottom: 1px dashed #999;"></div></td>';
-								$html .= '</tr>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm"><div style="height: 1px;"></div></td>';
-									$html .= '<td style="width: 43.05mm"><div style="height: 1px;"></div></td>';
-								$html .= '<tr style="vertical-align: middle;">';
-									$html .= '<td style="width: 43.05mm">&nbsp;</td>';
-									$html .= '<td style="width: 43.05mm"><b>'.__('TOTAL', 'sejoli-print-label').': '.sejolisa_price_format($value->grand_total).'</b></td>';
-								$html .= '</tr>';
-							$html .= '</tbody>';
-						$html .= '</table>';
-					$html .= '</div>';
-					if ($count%2 == 0)
-				    {
-						$html .= '<div style="clear:both"></div>';
-				    }
-				    $count++;
-				}
-				$html .= '</div>';
-		    	$html .= '</body></html>';
+			  	require_once( plugin_dir_path( __FILE__ ) . 'partials/sejoli-print-label-template.php' );
 
 			endif;
 
@@ -275,7 +169,7 @@ class Admin {
 		file_put_contents( $file_path, $output);
 		$invoice_url = SEJOLI_JNE_UPLOAD_URL . '/'. $file_name;
 		return wp_send_json($invoice_url);
-
+	
 	}
 
 }
